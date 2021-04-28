697d28ae27498750e25c9ba0664062bec354443b
Nicolas Bazille <oltodo@msn.com>
2021-04-28 19:37:25 +0200

    Introduce a basic branch removing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+import { prompt } from 'inquirer';
+import simpleGit from 'simple-git';
+
+(async function main() {
+  const git = simpleGit();
+  const { all: branches } = await git.branchLocal();
+
+  const { selectedBranches } = await prompt({
+    name: 'selectedBranches',
+    type: 'checkbox',
+    message: 'Select the branches to delete',
+    choices: branches,
+  });
+
+  await Promise.all(
+    selectedBranches.map((branch: string) => {
+      return git.deleteLocalBranch(branch, true);
+    }),
+  );
+})();

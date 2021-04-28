2e2534c43fb8e672181130f84fc7b5923f6f0ec9
Nicolas Bazille <oltodo@msn.com>
2021-04-28 21:40:04 +0200

    Handle merged branches

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import { prompt } from 'inquirer';
 import simpleGit from 'simple-git';
+import chalk from 'chalk';
 
 (async function main() {
   const git = simpleGit();
   const { all: branches } = await git.branchLocal();
 
+  if (branches.length === 1) {
+    throw new Error('You only have one branch');
+  }
+  if (branches.length === 0) {
+    throw new Error('No branch found');
+  }
+
+  const mainBranch = branches.includes('main') ? 'main' : 'master';
+
+  const { all: mergedBranches } = await git.branch(['--merged', mainBranch]);
+
   const { selectedBranches } = await prompt({
     name: 'selectedBranches',
     type: 'checkbox',
     message: 'Select the branches to delete',
-    choices: branches,
+    choices: branches
+      .filter((branch) => branch !== mainBranch)
+      .map((branch) => ({
+        name: mergedBranches.includes(branch)
+          ? `${branch} ${chalk.gray.italic('merged')}`
+          : branch,
+        value: branch,
+        toto: 'tutu',
+      })),
   });
 
-  await Promise.all(
-    selectedBranches.map((branch: string) => {
-      return git.deleteLocalBranch(branch, true);
-    }),
-  );
+  await git.deleteLocalBranches(selectedBranches, true);
 })();
